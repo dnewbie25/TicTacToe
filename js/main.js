9915996227d07b39aa8aff5386cec707cedf3d6a
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2021-10-12 10:49:07 -0500

    tictactoe finished, small visual issues left for modal

--- js/main.js
+++ js/main.js
@@ -1,53 +1,37 @@
 'use strict';
 
-// Player constructor
-// const Player = () => {
-//     const makeMove = (mark) => {
-//         document.addEventListener('click', e => {
-//             if (e.target.classList.contains('game-board__item')) {
-//                 const position = e.target.getAttribute('data-slot');
-//                 GameModule.updateArray(position, mark);
-//             }
-//         });
-//     }
-
-//     return {
-//         makeMove
-//     }
-// };
-
 // creating a game module that includes all the behaviors
 const GameModule = (function () {
     let boardArray = ['', '', '', '', '', '', '', '', ''];
     let turn = 'x';
     let mark = 'x';
-    
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    // check if the previous mark was an x or o and changes it
     const makeMove = () => {
         document.addEventListener('click', e => {
             if (e.target.classList.contains('game-board__item')) {
-                if (turn === 'x'){
+                if (turn === 'x') {
                     mark = 'x';
                     turn = 'o';
-                }else{
+                } else {
                     mark = 'o';
                     turn = 'x';
                 }
                 const position = e.target.getAttribute('data-slot');
-                GameModule.updateArray(position, mark);
+                updateArray(position, mark);
             }
         });
-
     }
-    makeMove();
-    // const changeTurn = ()=>{
-    //     if(turn === 'x'){
-    //         makeMove();
-    //     }else if(turn === 'o'){
-    //         makeMove();
-    //     }
-    // }
-    // changeTurn();
-    // reset and play Again buttons
+
     const displayBoard = () => {
         const gameFields = document.querySelectorAll('.game-board__item');
         gameFields.forEach(field => {
@@ -61,11 +45,11 @@ const GameModule = (function () {
         displayBoard();
     }
 
-    const closeModal =()=>{
+    const closeModal = () => {
         const modal = document.querySelector('.modal-background');
         const closeBtn = document.querySelector('.modal-card__close');
 
-        closeBtn.addEventListener('click', ()=>{
+        closeBtn.addEventListener('click', () => {
             boardArray = ['', '', '', '', '', '', '', '', ''];
             modal.style.display = 'none';
             mark = 'x';
@@ -74,19 +58,19 @@ const GameModule = (function () {
         });
     }
 
-    const reset = () =>{
+    const reset = () => {
         const reset = document.querySelector('.restart');
         const modal = document.querySelector('.modal-background');
         const replay = document.querySelector('.modal-card__play-again');
 
-        reset.addEventListener('click',()=>{
+        reset.addEventListener('click', () => {
             boardArray = ['', '', '', '', '', '', '', '', ''];
             mark = 'x';
             turn = 'x';
             displayBoard();
         });
 
-        replay.addEventListener('click',()=>{
+        replay.addEventListener('click', () => {
             boardArray = ['', '', '', '', '', '', '', '', ''];
             modal.style.display = 'none';
             mark = 'x';
@@ -94,10 +78,50 @@ const GameModule = (function () {
             displayBoard();
         });
     }
+    // check winner and return the value
+    const checkWinner = () => {
+        let whoWon = '';
+        for (let i = 0; i < winningConditions.length; i++) {
+            let a = boardArray[winningConditions[i][0]];
+            let b = boardArray[winningConditions[i][1]];
+            let c = boardArray[winningConditions[i][2]];
+            if (a != b || b != c) {
+                continue;
+            }
+
+            if (a === b && b === c) {
+                whoWon = a;
+                break;
+            }
+        }
+        if (!boardArray.includes('') && whoWon === '') {
+            return 'tie';
+        }
+        return whoWon;
+    };
+
+    const displayWinner = () => {
+        const modalCard = document.querySelector('.modal-background');
+        const modalMessage = document.querySelector('.modal-card__message');
+        document.addEventListener('click', () => {
+            if (checkWinner() === 'x') {
+                modalCard.style.display = 'flex';
+                modalMessage.textContent = "X's won this match!!";
+            } else if (checkWinner() === 'o') {
+                modalCard.style.display = 'flex';
+                modalMessage.textContent = "O's won this match!!";
+            } else if (checkWinner() === 'tie') {
+                modalCard.style.display = 'flex';
+                modalMessage.textContent = "It's a TIE!!";
+            }
+        })
+    }
+    // call the functions that should be running all the time
+    makeMove();
+    displayWinner();
     reset();
     closeModal();
     return {
-        updateArray,
-        turn
+        displayWinner
     }
 })();
